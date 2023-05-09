de319bf2c3f34220d0e13d17a4b46db0bf90e87b
root <root@DESKTOP-31HM7L6.localdomain>
2023-05-09 11:44:00 -0400

    setuup rules and help for both codes

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -4,3 +4,32 @@ import {getRandom, checkValid, checkWin, play, rps} from "../lib/rpsls.js"
 import minimist from "minimist";
 
 var arg = minimist(process.argv.slice(2));
+
+// setup help and rule messages
+
+const help = `Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+  -h, --help      display this help message and exit
+  -r, --rules     display the rules and exit
+Examples:
+  node-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}`;
+
+const rules = `Rules for Rock Paper Scissors:
+- Scissors CUTS Paper
+- Paper COVERS Rock
+- Rock CRUSHES Scissors`;
+
+// display help and rules if pormpted
+
+if (arg.h || arg.help) {
+    console.log(help);
+    process.exit(0);
+}
+
+if (arg.r || arg.rules) {
+    console.log(rules);
+    process.exit(0);
+}
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -4,3 +4,41 @@ import {getRandom, checkValid, checkWin, play, rpsls} from "../lib/rpsls.js"
 import minimist from "minimist";
 
 var arg = minimist(process.argv.slice(2));
+
+// setup help and rule messages
+
+const help = `Usage: node-rpsls [SHOT]
+Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+  -h, --help        display this help message and exit
+  -r, --rules       display the rules and exit
+Examples:
+  node-rpsls        Return JSON with single player RPSLS result.
+                    e.g. {"player":"rock"}
+  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}
+`;
+
+const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+- Scissors CUTS Paper
+- Paper COVERS Rock
+- Rock SMOOSHES Lizard
+- Lizard POISONS Spock
+- Spock SMASHES Scissors
+- Scissors DECAPITATES Lizard
+- Lizard EATS Paper
+- Paper DISPROVES Spock
+- Spock VAPORIZES Rock
+- Rock CRUSHES Scissors
+`;
+
+// display help and rules if pormpted
+
+if (arg.h || arg.help) {
+    console.log(help);
+    process.exit(0);
+}
+
+if (arg.r || arg.rules) {
+    console.log(rules);
+    process.exit(0);
+}
